c94bdf558cbaabe7820c580623161958d03da9f8
Nace Zavrtanik <zavrtaniknace@gmail.com>
2026-01-31 10:41:44 +0100

    nvim: common.lua reindented

--- nvim/lua/common.lua
+++ nvim/lua/common.lua
@@ -15,19 +15,19 @@ vim.o.breakindent = true
 vim.o.showbreak = "~"
 
 function _G.get_tabline()
-    local tabline = {}
-    local tabnr_max = vim.fn.tabpagenr("$")
-    local tabnr_sel = vim.fn.tabpagenr()
-    for tabnr = 1, tabnr_max do
-        if tabnr == tabnr_sel then
-            tabline[#tabline+1] = "%#TabLineSel#"
-        else
-            tabline[#tabline+1] = "%#TabLine#"
-        end
-        tabline[#tabline+1] = " " .. tabnr .. " "
+  local tabline = {}
+  local tabnr_max = vim.fn.tabpagenr("$")
+  local tabnr_sel = vim.fn.tabpagenr()
+  for tabnr = 1, tabnr_max do
+    if tabnr == tabnr_sel then
+      tabline[#tabline+1] = "%#TabLineSel#"
+    else
+      tabline[#tabline+1] = "%#TabLine#"
     end
-    tabline[#tabline+1] = "%#TabLineFill#"
-    return table.concat(tabline)
+    tabline[#tabline+1] = " " .. tabnr .. " "
+  end
+  tabline[#tabline+1] = "%#TabLineFill#"
+  return table.concat(tabline)
 end
 vim.o.tabline="%!v:lua.get_tabline()"
 
@@ -64,21 +64,21 @@ vim.keymap.set("n", "<Leader>cc", ":cclose<CR>")
 vim.keymap.set("n", "<Leader>cf", ":cfirst<CR>")
 vim.keymap.set("n", "<Leader>cl", ":clast<CR>")
 vim.api.nvim_create_autocmd(
-    { "QuickFixCmdPost" },
-    {
-        pattern = "grep",
-        callback = function()
-            vim.cmd("cwindow")
-        end,
-    }
+  { "QuickFixCmdPost" },
+  {
+    pattern = "grep",
+    callback = function()
+      vim.cmd("cwindow")
+    end,
+  }
 )
 
 vim.api.nvim_create_autocmd(
-    { "BufWritePre" },
-    {
-        pattern="*",
-        callback = function()
-            vim.cmd([[%s/\s\+$//e]])
-        end,
-    }
+  { "BufWritePre" },
+  {
+    pattern="*",
+    callback = function()
+      vim.cmd([[%s/\s\+$//e]])
+    end,
+  }
 )
