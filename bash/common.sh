48521d8776e0a4e6990c403b8b1ece7a93dcbfce
Nace Zavrtanik <zavrtaniknace@gmail.com>
2026-02-12 18:18:58 +0100

    bat added to dependencies and configured

--- bash/common.sh
+++ bash/common.sh
@@ -4,10 +4,14 @@ export PATH=~/dotfiles/bin:$PATH
 # Terminal prompt color
 . color --prompt > /dev/null
 
+export EDITOR=nvim
+
 alias tree='tree --gitfile ~/dotfiles/.gitignore_global'
 alias ncdu='ncdu --color dark'
 
-export EDITOR=nvim
+alias bat='batcat'
+export BAT_THEME=base16
+export BAT_STYLE=header,grid,numbers
 
 . ~/dotfiles/todo/lib/todolib.sh
 todolib_register_completion
@@ -18,3 +22,4 @@ export TODO_EDITOR='nvim -O'
 shlflib_register_completion
 export SHLF_DIR=~/dotfiles/shlf/.shelf/
 export SHLF_EDITOR='nvim -O'
+export SHLF_PAGER='batcat --style plain --color never'
--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 echo INIT: packages
 
 sudo apt-get update
-sudo apt-get install --yes git tmux alacritty ripgrep tree htop ncdu
+sudo apt-get install --yes git tmux alacritty ripgrep bat tree htop ncdu
 
 # Compile nvim
 if command -v nvim > /dev/null 2>&1; then
