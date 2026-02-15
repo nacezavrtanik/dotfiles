0afc5a9abb0568dbc9de2da03175c3164b7d0bdd
Nace Zavrtanik <zavrtaniknace@gmail.com>
2026-02-15 17:49:28 +0100

    fzf dependency added and configured

--- bash/common.sh
+++ bash/common.sh
@@ -13,6 +13,13 @@ alias bat='batcat'
 export BAT_THEME=base16
 export BAT_STYLE=header,grid,numbers
 
+export FZF_DEFAULT_OPTS='
+-i --layout=reverse --height=50% --no-info --no-scrollbar --cycle --scroll-off=5
+--prompt="?> " --pointer=" >" --border=none --preview-window=border-sharp
+--bind=ctrl-d:preview-half-page-down --bind=ctrl-u:preview-half-page-up
+--color=16,prompt:4,hl:4,pointer:2,hl+:2,border:2,gutter:-1,bg+:-1,fg+:-1
+'
+
 . ~/dotfiles/shlf/lib/shlflib.sh
 shlflib_register_completion
 export SHLF_DIR=~/dotfiles/shlf/.shelf/
