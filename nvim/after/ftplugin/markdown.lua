204d0c0791b90d152f6bc1d053ad7c86f580b767
Nace Zavrtanik <zavrtaniknace@gmail.com>
2026-02-27 10:57:30 +0100

    nvim: includeexpr defined for Markdown

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,3 +3,13 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = -1
 
 vim.opt_local.textwidth = 80
+
+vim.opt_local.suffixesadd:append(".md")
+vim.opt_local.isfname:append("32,58")
+function _G.get_includeexpr()
+  local fname = vim.v.fname
+  fname = fname:gsub(": ", "/")
+  fname = fname:gsub(" ", "_")
+  return fname
+end
+vim.opt_local.includeexpr = "v:lua.get_includeexpr()"
